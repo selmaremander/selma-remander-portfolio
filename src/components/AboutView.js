d59c1ca0e60600304c201749e3c19f451061b4c4
selmaremander <selma.remander@edu.turkuamk.fi>
2026-01-19 21:24:35 +0200

    about me changes + font

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -60,19 +60,23 @@ export default function AboutView({ onClose }) {
       </div>
 
       <div className="panel panel--about">
-        <header className="panel-header">
-          <h1>About me</h1>
-        </header>
-        <div className="panel-body">
-          <p>
-            Hi, I am a 3rd year student at Turku University of Applied Sciences.
-            Studying to become a game developer and technical artist. I have a
-            passion for creating immersive experiences and bringing virtual
-            worlds to life. In my free time, I enjoy exploring new technologies,
-            playing video games, and collaborating on creative projects. This
-            portfolio showcases some of my work and highlights my skills in game
-            development and technical artistry. Thank you for visiting!
-          </p>
+        <div className="about-sidebar">
+          <h1 className="about-title">about me</h1>
+        </div>
+
+        <div className="about-content">
+          <div className="panel-body">
+            <p>
+              Hi, I am a 3rd year student at Turku University of Applied
+              Sciences. Studying to become a game developer and technical
+              artist. I have a passion for creating immersive experiences and
+              bringing virtual worlds to life. In my free time, I enjoy
+              exploring new technologies, playing video games, and collaborating
+              on creative projects. This portfolio showcases some of my work and
+              highlights my skills in game development and technical artistry.
+              Thank you for visiting!
+            </p>
+          </div>
         </div>
       </div>
     </div>
